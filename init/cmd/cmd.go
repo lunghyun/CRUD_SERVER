679dc8835c888b6ce0415963b879b7263fc70a5b
lunghyun <nangmannsh@gmail.com>
2025-09-12 17:12:12 +0900

    flag를 통한 config path 관리하기

--- init/cmd/cmd.go
+++ init/cmd/cmd.go
@@ -1,4 +1,21 @@
 package cmd
 
+import (
+	"fmt"
+
+	"github.com/lunghyun/CRUD_SERVER/config"
+)
+
 type Cmd struct {
+	config *config.Config
+}
+
+func NewCmd(filepath string) *Cmd {
+	c := &Cmd{
+		config: config.NewConfig(filepath),
+	}
+
+	fmt.Println(c.config.Server.Port)
+
+	return c
 }
