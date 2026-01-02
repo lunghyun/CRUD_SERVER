42257bb4576998e6b10f6a48aedb78b2253242f9
lunghyun <nangmannsh@gmail.com>
2026-01-03 02:01:20 +0900

    DB connection : thread 안전을 위해 sync.Mutex로 연결 실패 시 재시도 가능한 싱글톤 패턴 구현

--- config/config.go
+++ config/config.go
@@ -18,19 +18,11 @@ type Config struct {
 
 func NewConfig(filePath string) (*Config, error) {
 	c := new(Config)
-	//var c *Config{} -> 이런 식이 좋음
-	//file, err := os.Open(filePath)
-	//if err != nil { // filepath를 못받아오면
-	//	return nil, fmt.Errorf("config 파일(%s) 에러: %w", filePath, err)
-	//}
-	//defer func() { _ = file.Close() }()
+
 	if err := godotenv.Load(filePath); err != nil { // .env 파일을 읽고 환경변수에 로드
 		return nil, fmt.Errorf(".env를 가져오지 못했습니다: %w", err)
 	}
 
-	//if err = toml.NewDecoder(file).Decode(c); err != nil { // toml이 디코딩이 안되면
-	//	return nil, fmt.Errorf("디코딩 에러: %w", err)
-	//}
 	if err := env.Parse(c); err != nil { // 환경 변수를 매핑
 		return nil, fmt.Errorf("매핑 실패: %w", err)
 	}
--- config/database.go
+++ config/database.go
@@ -1,9 +1,73 @@
 package config
 
+import (
+	"database/sql"
+	"fmt"
+	"sync"
+	"time"
+)
+
 type Database struct {
+	Driver   string `env:"DB_DRIVER"`
 	Host     string `env:"DB_HOST"`
 	Port     string `env:"DB_PORT"`
 	User     string `env:"DB_USER"`
 	Password string `env:"DB_PASSWORD"`
 	Name     string `env:"DB_NAME"`
+}
+
+var (
+	dbMutex      sync.Mutex
+	dbConnection *sql.DB
+)
+
+func (db *Database) NewConnection() (*sql.DB, error) {
+	dbMutex.Lock()
+	defer dbMutex.Unlock()
+
+	// 이미 연결 성공 -> 기존 커넥션 재사용
+	if dbConnection != nil {
+		return dbConnection, nil
+	}
+
+	// 1. dsn 설정
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?parseTime=true&charset=utf8mb4",
+		db.User,
+		db.Password,
+		db.Host,
+		db.Port,
+		db.Name,
+	)
+
+	// 2. db connection 생성
+	conn, err := sql.Open(db.Driver, dsn)
+	if err != nil {
+		return nil, fmt.Errorf("DB 연결 실패")
+	}
+
+	// 3. db connection pool 설정 (Spring HikariCP 기본값 적용)
+	conn.SetMaxOpenConns(10)                  // maximum-pool-size
+	conn.SetMaxIdleConns(10)                  // minimum-idle
+	conn.SetConnMaxLifetime(30 * time.Minute) // max-lifetime
+	conn.SetConnMaxIdleTime(10 * time.Minute) // idle-timeout
+
+	// 4. connection 검증 (실제 db와 연결되었는지)
+	if err = conn.Ping(); err != nil {
+		_ = conn.Close()
+		return nil, fmt.Errorf("DB ping 실패: %w", err)
+	}
+
+	// 5. 성공 시에만 전역 변수 할당
+	dbConnection = conn
+	fmt.Printf("DB 연결 성공: %s@%s:%s/%s\n", db.User, db.Host, db.Port, db.Name)
+
+	return dbConnection, nil
+}
+
+func (db *Database) Close() error {
+	if dbConnection != nil {
+		return dbConnection.Close()
+	}
+	return nil
 }
